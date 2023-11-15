8a53754ed361a935f98d7d4665b9a34c15a4590e
kashif890 <kashifnabeel.cs@gmail.com>
2023-11-15 10:39:07 +0500

    Hello world added

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("This is simple 10 Time Printer");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hi world... " + i);
+        }
+    }
+}
